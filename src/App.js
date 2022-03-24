32f8c38acdc8e18fbf07caf029906cfb3179ca8f
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-03-23 22:17:07 -0500

    Changed the main page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-//Import logo is rendering a file image on the screen
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-
-//App Function is returning a line of instruaction with a hyperlink to "Learn React"
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    let [ counter, changeCounter ] = useState(1)
+    return (
+        <h1>Declarative Counter {counter}</h1>
+    )
 }
 
 export default App;
